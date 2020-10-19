f06ef27f9a5e0c2201c9301873f771e3e9f1d640
Sean Kinsella <30955591+seankin@users.noreply.github.com>
2020-10-19 20:14:51 +0100

    Add files via upload

---
+++ arduino_sketch_01.cpp
@@ -0,0 +1,40 @@
+// Motor A connections
+int enA = 9;
+int in1 = 8;
+int in2 = 7;
+
+void setup() {
+  // Set all the motor control pins to outputs
+  pinMode(enA, OUTPUT);
+  pinMode(in1, OUTPUT);
+
+  
+  // Turn off motors - Initial state
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+
+}
+
+void loop() {
+  directionControl();
+  delay(200);
+}
+
+// This function lets you control spinning direction of motors
+void directionControl() {
+  // Set motors to maximum speed
+  // For PWM maximum possible values are 0 to 255
+  analogWrite(enA, 255);
+
+  // Turn on motor A & B
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  delay(1000);
+  
+  // Now change motor directions
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  delay(1000);
+  
+
+}
