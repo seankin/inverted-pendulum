86c347c6cb99f0565f9473b1b6cb0e36399a6809
Sean Kinsella <30955591+seankin@users.noreply.github.com>
2020-10-21 12:40:52 +0100

    Update encoder_arduino.ino

--- encoder_arduino.ino
+++ encoder_arduino.ino
@@ -1,48 +1,27 @@
-// Red - 5V
-// Black - GND
-const int encoder_1i = 2; // Green interrupt
-const int encoder_1b = 4; // White
-const int encoder_2i = 3; // Green interrupt
-const int encoder_2b = 5; // White interrupt
-const int limit_1 = 8; // 
-const int limit_2 = 9; // 
-long encoder_1 = 0;
-long encoder_1_change = 0;
-long encoder_2 = 0;
+#include <Encoder.h>
+
+Encoder myEnc1(2, 4);
+Encoder myEnc2(5, 7);
 
 void setup() {
-Serial.begin(115200, SERIAL_8E1);
-pinMode(encoder_1i, INPUT_PULLUP);
-pinMode(encoder_2i, INPUT_PULLUP);
-pinMode(encoder_1b, INPUT_PULLUP);
-pinMode(encoder_2b, INPUT_PULLUP);
-pinMode(limit_1, INPUT_PULLUP);
-pinMode(limit_2, INPUT_PULLUP);
-
-attachInterrupt(0, encoder1PinChange, CHANGE);
-attachInterrupt(1, encoder2PinChange, CHANGE);
+  Serial.begin(9600);
+  Serial.println("Basic Encoder Test:");
 }
 
+long oldPosition1  = -999;
+long oldPosition2  = -999;
 void loop() {
-//Serial.write(String(encoder_1) + "\t" + String(encoder_2) + "\t" + String(limit_1) + "\t" + String(limit_2))
-  if (Serial.available()) {
-    Serial.read();
-    Serial.print(encoder_1);
-    Serial.print("\t");
-    Serial.print(encoder_2);
-    Serial.print("\t");
-    Serial.print(!digitalRead(limit_1));
-    Serial.print("\t");
-    Serial.println(!digitalRead(limit_2));
-    Serial.flush();
+  long newPosition1 = myEnc1.read();
+  if (newPosition1 != oldPosition1) {
+    oldPosition1 = newPosition1;
+    Serial.print("Direction Cart Pole: ");
+    Serial.println(newPosition1);
+
+  long newPosition2 = myEnc2.read();
+  if (newPosition2 != oldPosition2) {
+    oldPosition2 = newPosition2;
+    Serial.print("Direction Pendulum: ");
+    Serial.println(newPosition2);
+    }
   }
-}
-
-void encoder1PinChange() {
-encoder_1 += digitalRead(encoder_1i) == digitalRead(encoder_1b) ? -1 : 1;
-encoder_1_change++;
-}
-
-void encoder2PinChange() {
-encoder_2 += digitalRead(encoder_2i) != digitalRead(encoder_2b) ? -1 : 1;
 }
